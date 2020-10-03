6a2cd1340a1c11a81b3638e77f3fd03ac32b5f21
omiya takakazu <omiya2015@gmail.com>
2020-10-03 20:32:39 -0300

    Acrescimo de rotinas

--- ProjetoOdontoClin/ProjetoOdontoClin/Controllers/DentistaController.cs
+++ ProjetoOdontoClin/ProjetoOdontoClin/Controllers/DentistaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjetoOdontoClin.Dados;
+using ProjetoOdontoClin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +15,20 @@ namespace ProjetoOdontoClin.Controllers
         {
             return View();
         }
+
+        public ActionResult cadDentista()
+        {
+            return View();
+        }
+        acoesDentista ac = new acoesDentista();
+        public ActionResult confCadDentista(modelDentista m)
+        {
+
+            ac.inserirDentista(m);
+            ViewBag.mensagem = "Cadastro Realizado com sucesso";
+            return View();
+        }
+
+
     }
 }
--- ProjetoOdontoClin/ProjetoOdontoClin/Dados/acoesDentista.cs
+++ ProjetoOdontoClin/ProjetoOdontoClin/Dados/acoesDentista.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProjetoOdontoClin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,18 @@ namespace ProjetoOdontoClin.Dados
 {
     public class acoesDentista
     {
+        conexao con = new conexao();
+
+        public void inserirDentista(modelDentista cm)
+        {
+            MySqlCommand cmd = new MySqlCommand("insert into tbDentista values(@CodDentista, @NomeDentista, @TelDentista,@EmailDentista)", con.MyConectarBD());
+
+            cmd.Parameters.Add("@codDentista", MySqlDbType.VarChar).Value = cm.CodDentista;
+            cmd.Parameters.Add("@nomeDentista", MySqlDbType.VarChar).Value = cm.NomeDentista;
+            cmd.Parameters.Add("@telDentista", MySqlDbType.VarChar).Value = cm.TelDentista;
+            cmd.Parameters.Add("@emailDentista", MySqlDbType.VarChar).Value = cm.EmailDentista;
+            cmd.ExecuteNonQuery();
+            con.MyDesconectarBD();
+        }
     }
 }
